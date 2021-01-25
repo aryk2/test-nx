6378e6217f3fd9e2884f21b120476f9f9ace7f33
aryeh <akempler-delugach@ignw.io>
2021-01-25 11:22:29 -0800

    initial commit

---
+++ libs/shared-ui/src/lib/shared-ui.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/* eslint-disable-next-line */
+export interface SharedUiProps {}
+
+export function SharedUi(props: SharedUiProps) {
+  return (
+    <div>
+      <h1>Welcome to shared-ui!</h1>
+    </div>
+  );
+}
+
+export default SharedUi;
